02df7a279f94045b7805bbeb2ce3d1f61db87d8b
mozart_chen <xiao_yaochen@126.com>
2025-05-10 20:59:44 +0800

    init：初始化空页面

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div>root layout</div>
         {children}
       </body>
     </html>
